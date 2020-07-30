7459a136b7236dfec388e6630bab253e4211bbfe
sushantchoudhary25 <sushant.11605194@lpu.in>
2020-07-30 11:11:24 +0530

    Added search fuction

--- music_app/sql/sql.py
+++ music_app/sql/sql.py
@@ -1,8 +1,10 @@
 create_table = "create table if not exists playlist(title varchar(255), artist varchar(255), " \
-               "album varchar(255), song varchar(255), url varchar(255) PRIMARY KEY);"
+               "album varchar(255), url varchar(255) PRIMARY KEY);"
 
-insert_statement = "INSERT INTO playlist VALUES('{}', '{}', '{}', '{}', '{}')"
+insert_statement = "INSERT INTO playlist VALUES('{}', '{}', '{}', '{}')"
 
 check_for_duplicate = "SELECT * from playlist where url = '{}'"
 
 fetch_songs = "SELECT * FROM playlist"
+
+search = "SELECT * FROM playlist where {} = '{}'"
--- music_app/views.py
+++ music_app/views.py
@@ -16,7 +16,7 @@ def insert_into_playlist(req):
 
         if len(res) == 0:
             query = sql.insert_statement.format(req.get("title"), req.get("artist"),
-                                                req.get("album"), req.get("song"), req.get("url"))
+                                                req.get("album"), req.get("url"))
             crsr.execute(query)
             app.logger.info("Inserted record successfully")
     except Exception as err:
@@ -26,7 +26,6 @@ def insert_into_playlist(req):
 @app.route("/upload", methods=["GET", "POST"])
 def upload_song():
     if request.method == "POST":
-
         insert_into_playlist(request.form)
 
         return redirect(request.url)
@@ -47,6 +46,36 @@ def view_playlist():
     playlist = fetch_all_songs()
     records = False
     if len(playlist) != 0:
-        print(playlist)
         records = True
     return render_template("playlist.html", playlist=playlist, records=records)
+
+
+def search_songs(category, value):
+    try:
+        crsr.execute(sql.search.format(category, value))
+        return crsr.fetchall()
+    except Exception as err:
+        raise err
+
+
+@app.route("/search-database", methods=["POST"])
+def search_database():
+    req = request.form
+
+    category = str(req.get("category")).lower()
+    value = req.get("search")
+
+    if category not in ["artist", "album", "title"]:
+        return render_template("search.html", message="category should be [Artist, Title, Album]")
+
+    playlist = search_songs(category=category, value=value)
+
+    if not playlist:
+        return render_template("search.html", message="No Record Found")
+
+    return render_template("playlist.html", playlist=playlist, records=True)
+
+
+@app.route("/search")
+def search():
+    return render_template("search.html", message=None)
